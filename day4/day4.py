46c183f34fffb225dd4c46adbd662a8180d6c6ee
Unknown <steveage.ch@gmail.com>
2019-12-04 16:03:33 +0000

    part 2 - testing ajaceny different which is better for part 1 also

--- day4/day4.py
+++ day4/day4.py
@@ -6,19 +6,22 @@ for test in range(input_lower, input_upper):
     incrementing = True
     two_ajacent = False
     test_str = str(test)
-    for digit_index in range(0, len(test_str)):
-        if digit_index > 0:
-            # test for ajacent
-            if test_str[digit_index] == test_str[digit_index-1]:
-                two_ajacent = True
+    for digit_index in range(1, len(test_str)):
+        # test for incresing only
+        if test_str[digit_index] < test_str[digit_index-1]:
+            incrementing = False
+            break
 
-            # test for incresing only
-            if test_str[digit_index] < test_str[digit_index-1]:
-                incrementing = False
-                break
+    # This would have been a better way of part 1 also
+    # Because the only incrementing rule, if duplicates exist in string they must be ajacent
+    for possible_aj_digit in range(0, 10):
+        num_digits = test_str.count(str(possible_aj_digit))
+        if num_digits == 2:
+            two_ajacent = True
 
     if incrementing and two_ajacent:
         possible_values += 1
         print(f"{test} is a possible value")
+
 
 print(f"there is {possible_values} possibles")
